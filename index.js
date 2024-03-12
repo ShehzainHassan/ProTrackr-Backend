499d9130e8ce35485d7c9aaf6f75f30d29e80d91
jGHostR <iqbalelahiasharfi@gmail.com>
2024-03-12 21:54:35 +0500

    Faculty DB Schema v1

---
+++ index.js
@@ -0,0 +1,349 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const dbSchema = require("./models/user");
+const User = dbSchema.User;
+const Group = dbSchema.Group;
+const FacultyIdea = dbSchema.FacultyIdea;
+const Faculty = dbSchema.Faculty;
+const URL = require("url");
+const cors = require("cors");
+
+const app = express();
+
+const jsonParser = bodyParser.json({limit: '5mb'});
+
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017", {
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function () {
+  console.log("DB Connection Successful!");
+});
+
+app.listen(3002, () => console.log("Example app is listening on port 3001."));
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
+
+app.post("/saveGroup", jsonParser, async (req, res) => {
+  const {
+    FYP_type,
+    Faculty_img,
+    Status,
+    Tags,
+    description,
+    email,
+    id,
+    img,
+    leader,
+    title,
+    advisor,
+  } = req.body;
+  console.log(req.body);
+  try {
+    const newGroup = new Group({
+      FYP_type,
+      Faculty_img: "",
+      Status,
+      Tags,
+      description,
+      email,
+      id,
+      img,
+      leader,
+      title,
+      advisor: "",
+    });
+    newGroup.save();
+    res.status(201).send(newGroup);
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+app.post("/signup", jsonParser, async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    rollNo,
+    cgpa,
+    email,
+    sdaGrade,
+    creditHours,
+    password,
+    photo,
+  } = req.body;
+
+  try {
+    const user = new User({
+      firstName,
+      lastName,
+      rollNo,
+      cgpa,
+      email,
+      sdaGrade,
+      creditHours,
+      password,
+      photo,
+    });
+    user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+// Faculty Signup
+app.post("/facultySignup", jsonParser, async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    roomNo,
+    interest_Tags,
+    curr_education,
+    password,
+    photo,
+  } = req.body;
+
+  try {
+    const faculty = new Faculty({
+      firstName,
+      lastName,
+      email,
+      roomNo,
+      interest_Tags,
+      curr_education,
+      password,
+      photo,
+    });
+    faculty.save();
+    res.status(201).send(faculty);
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+
+app.get("/allusers", jsonParser, async (req, res) => {
+  const query = URL.parse(req.url, true).query;
+  try {
+    const allusers = await User.find({});
+    if (allusers) {
+      res.status(200).send(allusers);
+    } else {
+      res.status(200).send(false);
+    }
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+
+// GET ALL Faculties
+app.get("/allFaculties", jsonParser, async (req, res) => {
+  const query = URL.parse(req.url, true).query;
+  try {
+    const allFaculties = await Faculty.find({});
+    if (allFaculties) {
+      res.status(200).send(allFaculties);
+    } else {
+      res.status(200).send(false);
+    }
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+app.get("/allGroups", jsonParser, async (req, res) => {
+  const query = URL.parse(req.url, true).query;
+  try {
+    const allGroups = await Group.find({});
+    if (allGroups) {
+      res.status(200).send(allGroups);
+    } else {
+      res.status(200).send(false);
+    }
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+app.get("/allFacultyIdeas", jsonParser, async (req, res) => {
+  const query = URL.parse(req.url, true).query;
+  try {
+    const allIdeas = await FacultyIdea.find({});
+    if (allIdeas) {
+      console.log(allIdeas);
+      res.status(200).send(allIdeas);
+    } else {
+      res.status(200).send(false);
+    }
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+app.post("/joinGroup", jsonParser, async (req, res) => {
+  const { id, email } = req.body;
+
+  const parsedId = parseFloat(String(id));
+
+  const group = await Group.findOne({ id: parsedId });
+  console.log(group);
+  if (group) {
+    const emails = group.email;
+
+    const emailExists = emails.some((e) => e.val == email);
+
+    if (emailExists) {
+      res.status(409).send("Email already exists");
+      return;
+    }
+
+    emails.push({ val: email });
+    group.db.collection("groups").updateOne(
+      { id: parsedId },
+      {
+        $set: { email: emails },
+        $currentDate: { lastModified: true },
+      }
+    );
+    res.status(201).send(group);
+  }
+});
+
+app.get("/userdetails", jsonParser, async (req, res) => {
+  const query = URL.parse(req.url, true).query;
+  try {
+    const userDetails = await User.findOne({ email: query.email });
+    if (userDetails) {
+      res.status(200).send({
+        firstName: userDetails.firstName,
+        lastName: userDetails.lastName,
+        rollNo: userDetails.rollNo,
+        email: userDetails.email,
+        cgpa: userDetails.cgpa,
+        sdaGrade: userDetails.sdaGrade,
+        creditHours: userDetails.creditHours,
+        photo: userDetails.photo,
+      });
+    } else {
+      res.status(200).send(false);
+    }
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+//FacultyDetails
+app.get("/facultyDetails", jsonParser, async (req, res) => {
+  const query = URL.parse(req.url, true).query;
+  try {
+    const facultyDetails = await Faculty.findOne({ email: query.email });
+    if (facultyDetails) {
+      res.status(200).send({
+        firstName: facultyDetails.firstName,
+        lastName: facultyDetails.lastName,
+        email: facultyDetails.email,
+        roomNo: facultyDetails.roomNo,
+        interest_Tags: facultyDetails.interest_Tags,
+        curr_education: facultyDetails.curr_education,
+        photo: facultyDetails.photo,
+      });
+    } else {
+      res.status(200).send(false);
+    }
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+app.post("/login", jsonParser, async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  try {
+    const loggedUser = await User.findOne({ email, password });
+    console.log(loggedUser);
+    if (loggedUser) {
+      console.log("Successfully Logged In");
+      res.status(201).send({ email, password });
+    } else {
+      console.error("Invalid email or password");
+      res.status(201).send(false);
+    }
+    // const credentials = db.collection('users').find({}, {projection: {email: 1, password: 1, _id:0 }});
+    // let isValidCredentials = false;
+    // credentials.forEach(document => {
+    //     if (email === document.email && password === document.password){
+    //         console.log("Successfully Logged In");
+    //         // console.log(db.collection('users').findOne({}, (error, result) => {console.log(result)}))
+    //         res.status(201).send({email, password});
+    //         return;
+    //     }
+    // })
+    // console.log("Invalid Credentials");
+    // res.status(201).send({});
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+// Faculty Login 
+app.post("/facultyLogin", jsonParser, async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  try {
+    const loggedFaculty = await Faculty.findOne({ email, password });
+    console.log(loggedFaculty);
+    if (loggedFaculty) {
+      console.log("Faculty Successfully Logged In ~iqbal");
+      res.status(201).send({ email, password });
+    } else {
+      console.error("Invalid email or password");
+      res.status(201).send(false);
+    }
+    // const credentials = db.collection('users').find({}, {projection: {email: 1, password: 1, _id:0 }});
+    // let isValidCredentials = false;
+    // credentials.forEach(document => {
+    //     if (email === document.email && password === document.password){
+    //         console.log("Successfully Logged In");
+    //         // console.log(db.collection('users').findOne({}, (error, result) => {console.log(result)}))
+    //         res.status(201).send({email, password});
+    //         return;
+    //     }
+    // })
+    // console.log("Invalid Credentials");
+    // res.status(201).send({});
+  } catch (err) {
+    res.status(422).send(err);
+    console.log(err);
+  }
+});
+
+// // Defining User model
+// const User = mongoose.model('User', User.userSchema);
+
+// User.createCollection().then(function (collection) {
+//     console.log('Collection is created!');
+// });
---
+++ models/user.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  rollNo: String,
+  email: String,
+  cgpa: Number,
+  sdaGrade: String,
+  creditHours: Number,
+  password: String,
+  photo: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+User.createCollection().then(function (collection) {
+  console.log("Student Collection is created!");
+});
+
+const FacultySchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  roomNo: String,
+  interest_Tags: String,
+  curr_education: String,
+  password: String,
+  photo: String,
+})
+
+const Faculty = mongoose.model("Faculty", FacultySchema);
+
+Faculty.createCollection().then(function (collection){
+  console.log("Faculty Collection is created ~ from Iqbal");
+});
+
+// module.exports = mongoose.model("User", userSchema);
+
+const groupSchema = new mongoose.Schema({
+  FYP_type: String,
+  Faculty_img: String,
+  Status: String,
+  Tags: [{ val: String }],
+  description: String,
+  email: [{ val: String }],
+  id: Number,
+  img: [{ val: String }],
+  leader: String,
+  title: String,
+  advisor: String,
+});
+
+const Group = mongoose.model("Group", groupSchema);
+
+Group.createCollection().then(function (collection) {
+  console.log("Group Collection is created!");
+});
+
+const FacultyIdeaSchema = new mongoose.Schema({
+  FYP_type: String,
+  Faculty_img: String,
+  Tags: [{ val: String }],
+  description: String,
+  id: Number,
+  title: String,
+  contact: String,
+  advisor: String,
+});
+
+const FacultyIdea = mongoose.model("FacultyIdea", FacultyIdeaSchema);
+
+FacultyIdea.createCollection().then(function (collection) {
+  console.log("Faculty Idea Schema is created!");
+});
+
+exports.User = User;
+exports.Group = Group;
+exports.FacultyIdea = FacultyIdea;
+
+exports.Faculty = Faculty;
